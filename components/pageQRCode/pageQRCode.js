4fae11faa0cb4a5a8b2a39d5dc78f49ccb864407
520wsl <395548460@qq.com>
2018-10-26 19:21:31 +0800

    first commit

---
+++ components/pageQRCode/pageQRCode.js
@@ -0,0 +1,24 @@
+// components/pageQRCode/pageQRCode.js
+// 分享 页面 二维码 组件
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
